6c137cb2f132b515e0fefee77c4aadd024168ef7
Muzamil <muzamiliqbalganaie@gmail.com>
2025-04-27 00:59:54 +0530

    'arcjet'

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import userRouter from './routes/user.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 
 
 const app = express();
@@ -16,10 +17,12 @@ const app = express();
 app.use(express.json()); // Parse incoming JSON requests
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests  
 app.use(cookieParser()); // Parse cookies from the request headers
+app.use(arcjetMiddleware);
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
 
 
 app.use(errorMiddleware);
---
+++ middlewares/arcjet.middleware.js
@@ -0,0 +1,21 @@
+import aj from '../config/arcjet.js';
+
+const arcjetMiddleware = async (req, res, next) => {
+    try {
+        const decision = await aj.protect(req, { requested: 5 }); // Deduct 5 tokens from the bucket
+        console.log("Arcjet decision", decision);
+
+        if (decision.isDenied()) {
+            if (decision.reason.isRateLimit())
+                return res.status(429).json({ error: "Too Many Requests" });
+            if (decision.reason.isBot())
+                return res.status(403).json({ error: "No bots allowed" });
+            return res.status(403).json({ error: "Forbidden Access Denied" });
+        }
+    } catch (error) {
+        console.log(`Arcjet middleware error", ${error}`)
+        next(error);
+    }
+}
+
+export default arcjetMiddleware;
